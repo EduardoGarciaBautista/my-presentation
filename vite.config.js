a69712f5399dac40f2a9c8b2d9a06207fac04722
Eduardo G <proyectosngegb@gmail.com>
2023-10-26 18:32:44 -0600

    Update vite

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/my-presentation/",
   plugins: [react()],
   server: {
     host: true
   },
-  base: "/my-presentation/"
 })
